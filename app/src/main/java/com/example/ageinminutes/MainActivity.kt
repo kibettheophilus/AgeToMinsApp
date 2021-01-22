b52de79e9d25b208b878dea41941bd6be2841489
Kibet Theophilus <kibettheophilus@gmail.com>
2021-01-22 18:28:46 +0300

    Added textViews and a button

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.ageinminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDatePicker.setOnClickListener {
+            //Toast.makeText(context:this)
+        //encountered an error
+        }
+
+    }
+
+    fun clikDatePicker(view: View){
+        //DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->  })
+        //error 2
     }
 }
